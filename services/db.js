ffd443433a35e189be99b8de98ebf7368dfa2cec
Vedansh Mehra <vedanshofficial@gmail.com>
2023-06-11 15:51:05 +0530

    setup connection pooling in db config

--- services/db.js
+++ services/db.js
@@ -1,13 +1,27 @@
 const mysql = require("mysql");
 require("dotenv").config();
 
-const connection = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DB,
-});
+let connection;
 
-connection.connect();
+function startConnection() {
+  console.error("CONNECTING");
+  connection = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB,
+  });
+  connection.connect((err) => {
+    if (err) {
+      console.error("CONNECT FAILED", err.code);
+      startConnection();
+    } else console.log("CONNECTED");
+  });
+  connection.on("error", function (err) {
+    if (err.fatal) startConnection();
+  });
+}
+
+startConnection();
 
 module.exports = { connection };
