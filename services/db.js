7dcfd530ba28ac5c4719601d4d9643db94e2cae9
Vedansh Mehra <vedanshofficial@gmail.com>
2023-06-11 16:40:48 +0530

    removed mysql and use mysql2

--- services/db.js
+++ services/db.js
@@ -1,10 +1,11 @@
-const mysql = require("mysql");
+const mysql = require("mysql2");
 require("dotenv").config();
 
 function startConnection() {
   console.error("CONNECTING");
   connection = mysql.createConnection({
     host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DB,
